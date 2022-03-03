67eda3cad33115a57f5f5d70eb4cf7e1feb70068
Sachin Yadav <sachin.yadav@csinfocomm.com>
2022-03-03 23:16:05 +0530

    TopBar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,13 +3,23 @@ const TopBar = () => {
     return(
         <div className="top">
             <div className="topLeft">
-            <i class="fa-brands fa-facebook-square"></i>
-            <i class="fa-brands fa-twitter-square"></i>
-            <i class="fa-brands fa-pinterest-square"></i>
-            <i class="fa-brands fa-instagram-square"></i>
+            <i className="topIcon fa-brands fa-facebook-square"></i>
+            <i className="topIcon fa-brands fa-twitter-square"></i>
+            <i className="topIcon fa-brands fa-pinterest-square"></i>
+            <i className="topIcon fa-brands fa-instagram-square"></i>
             </div>
-            <div className="topCenter">c</div>
-            <div className="topRight">r</div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topImg" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg" alt="" />
+                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+            </div>
         </div>
     )
 }
